a687f3dd4938a34d73d4301156525756537494b5
Bogdan Baghiu <bogdantrabajo@gmail.com>
2018-08-05 22:27:17 +0200

    Module 2 - Activity exercise

---
+++ Modulo_2/Modulo2Ejercicio1Actividades/app/src/main/java/android/ejercicios/upv/modulo2ejercicio1actividades/ThirdActivity.java
@@ -0,0 +1,14 @@
+package android.ejercicios.upv.modulo2ejercicio1actividades;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class ThirdActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_activity);
+    }
+}
